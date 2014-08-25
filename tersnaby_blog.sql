132ccd984b502fba942a22cf4e0269a7a285f4dc
D-N-D <dimitar.nikolaev.dimitrov@gmail.com>
2014-08-25 17:46:35 +0300

    - updating some db default values

--- tersnaby_blog.sql
+++ tersnaby_blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.8
+-- version 4.2.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 21 авг 2014 в 16:58
--- Версия на сървъра: 5.1.66-rel14.2-log
--- PHP Version: 5.4.23
+-- Generation Time: Aug 25, 2014 at 04:42 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,85 +17,128 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `tersnaby_blog`
+-- Database: `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `comments`
+-- Table structure for table `comments`
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`comment_id` int(11) unsigned NOT NULL,
   `comment_content` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `comment_date` datetime NOT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  `post_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `comment_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) unsigned DEFAULT '0',
+  `user_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'guest',
+  `post_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=79 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `posts`
+-- Table structure for table `posts`
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
-  `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`post_id` int(11) unsigned NOT NULL,
   `post_count` int(10) NOT NULL DEFAULT '0',
   `post_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `post_desc` text COLLATE utf8_unicode_ci,
   `post_cont` text COLLATE utf8_unicode_ci,
   `post_tags` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `post_date` datetime DEFAULT NULL,
-  `user_id` int(11) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
-
---
--- Схема на данните от таблица `posts`
---
-
-INSERT INTO `posts` (`post_id`, `post_count`, `post_title`, `post_desc`, `post_cont`, `post_date`, `user_id`) VALUES
-(1, 0, 'dwaaw', 'dwawa', 'dwadwawdawawd', '2014-08-20 17:55:46', 0),
-(2, 0, 'daw', 'dwaaw', 'gdsggsdgdgssg', '2014-08-20 17:58:40', 0);
+  `post_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `tags`
+-- Table structure for table `tags`
 --
 
 CREATE TABLE IF NOT EXISTS `tags` (
-  `tag_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `tag_count` int(11) unsigned NOT NULL ,
-  `tag_title` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+`tag_id` int(11) unsigned NOT NULL,
+  `tag_count` int(11) unsigned DEFAULT '1',
+  `tag_title` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`user_id` int(11) unsigned NOT NULL,
   `user_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_mail` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_pass` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_rights` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+  `user_rights` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=79 ;
 
 --
--- Схема на данните от таблица `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_mail`, `user_pass`, `user_rights`) VALUES
-(1, 'Mitko', 'mitko@abv.bg', '123456', 'admin');
+(0, 'guest', 'guest@guest.com', '1234567Ok', NULL),
+(1, 'Mitko', 'mitko@abv.bg', '123456', 'admin'),
+(2, 'DND17', 'prince_of_persia@abv.bg', '$2a$12$mN4UOLssCnGVnMdFWCHSTOEYiIsSK2gWPk0JeoqSwPKokh0RnQ7Yi', 'user');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+ ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+ ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+ ADD PRIMARY KEY (`tag_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+MODIFY `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=79;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+MODIFY `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+MODIFY `tag_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=79;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
