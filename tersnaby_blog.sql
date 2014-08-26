8919d887e7a78f96c2d70a17ece4b838ec526e98
D-N-D <dimitar.nikolaev.dimitrov@gmail.com>
2014-08-26 18:13:24 +0300

    - update guest/user mail appearance in comments

--- tersnaby_blog.sql
+++ tersnaby_blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.7
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 
--- Версия на сървъра: 5.6.16
--- PHP Version: 5.5.11
+-- Host: localhost
+-- Generation Time: Aug 26, 2014 at 05:11 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,101 +17,166 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `glava`
+-- Database: `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `comments`
+-- Table structure for table `comments`
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`comment_id` int(11) unsigned NOT NULL,
   `comment_content` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `comment_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(11) unsigned DEFAULT '0',
+  `user_mail` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'guest',
-  `post_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `post_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=18 ;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `comment_content`, `comment_date`, `user_id`, `user_mail`, `user_name`, `post_id`) VALUES
+(15, 'test', '2014-08-26 18:04:07', 0, '', 'guest', 40),
+(16, 'breh', '2014-08-26 18:04:45', 0, 'franz_dimitrov@hotmail.com', 'guest', 40),
+(17, 'ythgaskjdhkasjhdkasjhdksajhds', '2014-08-26 18:06:12', 0, 'prince_of_persia@abv.bg', 'DND17', 40);
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `posts`
+-- Table structure for table `posts`
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
-  `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`post_id` int(11) unsigned NOT NULL,
   `post_count` int(10) NOT NULL DEFAULT '0',
   `post_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `post_desc` text COLLATE utf8_unicode_ci,
   `post_cont` text COLLATE utf8_unicode_ci,
   `post_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(11) unsigned NOT NULL DEFAULT '0',
-  `post_tags` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=39 ;
+  `post_tags` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=41 ;
 
 --
--- Схема на данните от таблица `posts`
+-- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`post_id`, `post_count`, `post_title`, `post_desc`, `post_cont`, `post_date`, `user_id`, `post_tags`) VALUES
 (35, 0, 'kljhkhkj', 'khlfkgjlgkggkgk', 'lkjflkgjlgkj', '2014-08-26 13:18:48', 0, ' 12 13'),
-(36, 0, 'hdfzhzf', 'zdvhzhhh', 'dfhnvdnv', '2014-08-26 13:19:01', 0, ' 13 12 14'),
+(36, 1, 'hdfzhzf', 'zdvhzhhh', 'dfhnvdnv', '2014-08-26 13:19:01', 0, ' 13 12 14'),
 (37, 0, 'dwaa', 'dawawd', 'dawdawawa', '2014-08-26 13:47:30', 0, ' 1 2 3'),
-(38, 0, 'dawdwada', 'dawdwaw', 'dawdawwd', '2014-08-26 13:47:41', 0, ' 3 2 1');
+(38, 0, 'dawdwada', 'dawdwaw', 'dawdawwd', '2014-08-26 13:47:41', 0, ' 3 2 1'),
+(39, 0, 'a dano', 'lhlklk', 'kjlkdjfkdf', '2014-08-26 15:23:38', 0, ' 1 2 test2'),
+(40, 0, 'hhhhh', 'lllll', 'khjhjhjh', '2014-08-26 15:24:42', 0, ' test1 test2 3 4 5');
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `tags`
+-- Table structure for table `tags`
 --
 
 CREATE TABLE IF NOT EXISTS `tags` (
-  `tag_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`tag_id` int(11) unsigned NOT NULL,
   `tag_count` int(11) unsigned NOT NULL DEFAULT '1',
-  `tag_title` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`tag_id`),
-  UNIQUE KEY `tag_title` (`tag_title`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=108 ;
+  `tag_title` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=116 ;
 
 --
--- Схема на данните от таблица `tags`
+-- Dumping data for table `tags`
 --
 
 INSERT INTO `tags` (`tag_id`, `tag_count`, `tag_title`) VALUES
 (97, 2, '12'),
 (98, 2, '13'),
 (101, 1, '14'),
-(102, 2, '1'),
-(103, 2, '2'),
-(104, 2, '3');
+(102, 3, '1'),
+(103, 3, '2'),
+(104, 3, '3'),
+(110, 2, 'test2'),
+(111, 1, 'test1'),
+(114, 1, '4'),
+(115, 1, '5');
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`user_id` int(11) unsigned NOT NULL,
   `user_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_mail` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_pass` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_rights` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+  `user_rights` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
--- Схема на данните от таблица `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_mail`, `user_pass`, `user_rights`) VALUES
-(1, 'Mitko', 'mitko@abv.bg', '123456', 'admin');
+(0, 'guest', NULL, NULL, NULL),
+(1, 'Mitko', 'mitko@abv.bg', '123456', 'admin'),
+(2, 'DND17', 'prince_of_persia@abv.bg', '$2a$12$HnuV.tlkfbde7m.23SkeM.h9BOQgWsRyuVfIEUpeGhTHYC9NEOChy', 'user');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+ ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+ ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+ ADD PRIMARY KEY (`tag_id`), ADD UNIQUE KEY `tag_title` (`tag_title`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+MODIFY `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+MODIFY `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+MODIFY `tag_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=116;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- tp/viewpost.php
+++ tp/viewpost.php
@@ -1,36 +1,34 @@
+<?php
+    include "database.php";
+
+
+    $postId = $_GET['id'];
+
+    $query="SELECT post_title, post_desc, post_cont, post_date, post_count FROM posts WHERE post_id = '$postId'";
+
+    $sth =  $db->query($query);
+    $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+    $title = $row['post_title'];
+    $desc = $row['post_desc'];
+    $cont = $row['post_cont'];
+    $date = $row['post_date'];
+    $count = $row['post_count'];
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>!!!Post title - to add dynamic title according to the title!!!</title>
+    <title><?=$title?></title>
     <meta charset="UTF-8">
     <link type="text/css" rel="stylesheet" href="Styles/viewpost.css"/>
 </head>
 <body>
-<header>
-    <h1><a href="index.php">MY BLOG</a></h1>
-    <a href="register.php">Register</a>
-    <a href="login.php">Login</a>
-    <a href="addpost.php">New post</a>
-</header>
+<?php
+    include "header.php";
+?>
 <main>
     <section id="blog-post">
         <?php
-        include "database.php";
-        include "header.php";
-
-        $postId = $_GET['id'];
-
-        $query="SELECT post_title, post_desc, post_cont, post_date, post_count FROM posts WHERE post_id = '$postId'";
-
-        $sth =  $db->query($query);
-        $row = $sth->fetch(PDO::FETCH_ASSOC);
-
-            $title = $row['post_title'];
-            $desc = $row['post_desc'];
-            $cont = $row['post_cont'];
-            $date = $row['post_date'];
-            $count = $row['post_count'];
-
             if(!isset($_COOKIE[$postId])){
                 $sqlCount = "UPDATE posts SET post_count = post_count + 1 WHERE post_id = '$postId'";
                 $query = $db->prepare($sqlCount);
@@ -58,53 +56,45 @@
 
         $user_id = $_SESSION['user_id'];
         $user_name = $_SESSION['user_name'];
+        $user_mail = $_SESSION['user_mail'];
+
+        $userCredentials = true;
 
         if (!$user_name && !$user_id) {
-            $user_id = 0;
-            $user_name = 'guest';
-
         ?>
 
             <label for="user">Author:</label>
-            <input type="text" name="user" id="user"/>
+            <input type="text" name="user" id="user" placeholder="guest"/>
             <label for="email">E-mail:</label>
             <input type="email" name="email" id="email"/>
-
-
         <?php
             $user_name = trim($_POST['user']);
             $user_mail = trim($_POST['email']);
 
-           /* $sql = "INSERT INTO users ( user_name, user_mail)VALUES ('$user_name','$user_mail')";
-
-            $query = $db->prepare($sql);
-
-            $query->execute(array(
-                ':user_name' => $user_name,
-                ':user_mail' => $user_mail
-            ));
-           */
+            $user_id = 0;
+            $user_name = 'guest';
         }
 
         ?>
             <input id="submit" type="submit" value="Submit">
+
         </form>
         </section>
         <section id="published-comments">
         <?php
         if (isset($comment) && strlen($comment) > 0)
         {
-           $sql = "INSERT INTO comments ( comment_content, post_id, user_name) VALUES ( '$comment', '$postId', '$user_name')";
+           $sql = "INSERT INTO comments ( comment_content, post_id, user_name, user_mail) VALUES ( '$comment', '$postId', '$user_name', '$user_mail')";
 
             $query = $db->prepare( $sql );
 
-            $query->execute(array(':comment_content' => $comment, ':post_id' =>$postId, ':user_name' => $user_name));
+            $query->execute(array(':comment_content' => $comment, ':post_id' =>$postId, ':user_name' => $user_name, '"user_mail' => $user_mail));
 
             header('Location: viewpost.php?id='.$postId);
         }
 
 
-        $query="SELECT comment_content, post_id, user_name, comment_date FROM comments ORDER BY comment_id DESC";
+        $query="SELECT comment_content, post_id, user_name, user_mail, comment_date FROM comments ORDER BY comment_id DESC";
 
         $commentsValues = $db->query($query);
 
@@ -114,12 +104,21 @@
             $dbText = htmlentities($row['comment_content']);
             $dbPostId = $row['post_id'];
             $dbUserName = $row['user_name'];
+            $dbUserMail = $row['user_mail'];
+            if(!$dbUserMail) {
+                $dbUserMail = null;
+            }
+            else
+            {
+                $dbUserMail = "($dbUserMail)";
+            }
             $dbCommentDate = $row['comment_date'];
+
 
             if ($dbPostId == $postId)
             {
             echo "<div class='comment-entry'>";
-            echo "<p>Commented by $dbUserName on $dbCommentDate</p>";
+            echo "<p>Commented by $dbUserName $dbUserMail on $dbCommentDate</p>";
             echo "<p>$dbText</p>";
             echo "</div>";
             }
