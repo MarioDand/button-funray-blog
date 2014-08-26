bc84d362b340e302547b8f50b4d174e1c5559c9a
Aleksandrov <emil.aleksandrov87@gmail.com>
2014-08-26 15:57:10 +0300

    edit header.php

--- tp/header.php
+++ tp/header.php
@@ -22,14 +22,31 @@
     </head>
     <body>
     <header>
-        <h1>MY BLOG</h1>
-        <a href="register.php">Register</a>
-        <a href="login.php">Login</a>
-        <a href="addpost.php">New post</a>
+        <h1><a href="index.php">BLOG</a></h1>
+
+        <?php if(isset($_SESSION['user_name']) && $_SESSION['user_name'] &&($_SESSION['user_rights']==='user')){
+?>
+        <a>Hello <?php echo $_SESSION['user_name']; ?> </a>
+            <a href="logout.php">Log out</a>
+       <?php }elseif(isset($_SESSION['user_name']) && $_SESSION['user_name']&&($_SESSION['user_rights']==='admin')){
+            ?>
+            <a>Hello <?php echo $_SESSION['user_name']; ?> </a>
+            <a href="addpost.php">Add post</a>
+            <a href="logout.php">Log out</a>
+        <?php
+        }else{
+            ?>
+            <a href="register.php">Register</a>
+            <a href="login.php">Login</a>
+            <a>Hello Guest </a>
+                <?php
+        }
+        ?>
+
     </header>
     </body>
     </html>
 
 <?php
-
 include "database.php";
+?>
