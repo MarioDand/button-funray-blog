1eceec5963790f1df095bc6277335dab4c29b643
D-N-D <dimitar.nikolaev.dimitrov@gmail.com>
2014-08-26 19:46:53 +0300

    - adding footer.php

---
+++ tp/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <link type="text/css" rel="stylesheet" href="Styles/footer.css"/>
+    <div id="info">
+        <div class="footer-links">
+            <a href="#" target="_blank">За БГ-ТАТИ</a> |
+            <a href="#" target="_blank">Помощ</a> |
+            <a href="#" target="_blank">F.A.Q.</a> |
+            <a href="#" target="_blank">Поверителност</a>
+            <br><br>
+            <div class="logo">
+                <a target="_blank" href="index.php"><img src="Images/logo.png" width="906" height="234" /></a>
+            </div>
+            <br>
+            <div class="clinks"><a target="_blank" href="http://www.softuni.bg">SoftUni</a> &copy; 2014 Всички права запазени. <a href="#">Контакти</a></div>
+
+
+        </div>
+</footer>
