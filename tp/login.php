e71b81c5221f3cc062a0054aedcaf64a08bafb85
Aleksandrov <emil.aleksandrov87@gmail.com>
2014-08-26 15:55:39 +0300

    Input user_rights in cookies

--- tp/login.php
+++ tp/login.php
@@ -23,6 +23,11 @@
 include "database.php";
 include "header.php";
 
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+
 if (isset($_POST['user']) && isset($_POST['pass'])) {
 
     $Name = htmlentities(trim($_POST['user']));
@@ -30,7 +35,7 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
 
     if (($Name !== '') && ($Password !== '')) {
 
-        $query = "SELECT user_id, user_name, user_mail, user_pass FROM users where user_name = '$Name'";
+        $query = "SELECT * FROM users where user_name = '$Name'";
         $sth = $db->query($query);
         $row = $sth->fetch(PDO::FETCH_ASSOC);
         $checkPass = crypt($Password, $row['user_pass']);
@@ -39,8 +44,8 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
             $_SESSION['user_id'] = $row['user_id'];
             $_SESSION['user_name'] = $row['user_name'];
             $_SESSION['user_mail'] = $row['user_mail'];
+            $_SESSION['user_rights'] = $row['user_rights'];
 
-            echo "Successfully login";
             header('Location: index.php');
         } else {
             echo "Sorry... You entered wrong Username or Password... Please retry...";
